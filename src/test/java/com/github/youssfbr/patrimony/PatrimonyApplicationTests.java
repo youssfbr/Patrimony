e4fe43462a2c4da86b2d63ce2674e3eb7ad14da1
Alisson Youssf <youssfbr@gmail.com>
2023-11-28 20:04:27 -0300

    Project created

---
+++ src/test/java/com/github/youssfbr/patrimony/PatrimonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.patrimony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrimonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
